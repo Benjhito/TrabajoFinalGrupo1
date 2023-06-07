bd14e0cd353abd2e0e86c5e3f34e5580256bc2ff
BejaranoLourdes <bejaranolourdes55@gmail.com>
2023-06-07 16:05:16 -0300

    Adicion de clase Especialidad, controlador y servicio - Bejarano Lourdes
    - Creacion del objeto Especialidad
    - Adicion de EspecialidadController
    - Adicion de EspecialidadService

--- src/main/java/ar/edu/unju/edm/controller/EspecialidadController.java
+++ src/main/java/ar/edu/unju/edm/controller/EspecialidadController.java
@@ -1,5 +1,52 @@
 package ar.edu.unju.edm.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.beans.factory.annotation.Autowired;
 
+import ar.edu.unju.edm.model.Especialidad;
+import ar.edu.unju.edm.service.EspecialidadService;
+
+@Controller
 public class EspecialidadController {
-
+	@Autowired
+	Especialidad especialidad;
+	@Autowired
+	EspecialidadService especialidadService;
+	
+	@GetMapping("/formEspecialidad")
+	public ModelAndView cargarFormEspecialidad() {
+		
+		ModelAndView vistaFormEspecialidad = new ModelAndView("formEspecialidad");
+		vistaFormEspecialidad.addObject("especialidad", especialidad);
+		
+		return vistaFormEspecialidad;
+	}
+	
+	@PostMapping("/formEspecialidad")
+	public ModelAndView mostrarFormEspecialidad() {
+		
+		ModelAndView vistaFormEspecialidad = new ModelAndView("formEspecialidad");
+		vistaFormEspecialidad.addObject("especialidad", especialidad);
+		
+		return vistaFormEspecialidad;
+	}
+	
+	@GetMapping("/listaEspecialidades")
+	public ModelAndView cargarListaEspecialidades() {
+		
+		ModelAndView vistaListaEspecialidades = new ModelAndView("listaEspecialidades");
+		
+		return vistaListaEspecialidades;
+	}
+	
+	@PostMapping("/listaEspecialidades")
+	public ModelAndView mostrarListaEspecialidades() {
+		
+		ModelAndView vistaListaEspecialidades = new ModelAndView("listaEspecialidades");
+		
+		return vistaListaEspecialidades;
+	}
 }
--- src/main/java/ar/edu/unju/edm/service/EspecialidadService.java
+++ src/main/java/ar/edu/unju/edm/service/EspecialidadService.java
@@ -1,5 +1,15 @@
 package ar.edu.unju.edm.service;
+import java.util.ArrayList;
+import org.springframework.stereotype.Service;
 
-public class EspecialidadService {
+import ar.edu.unju.edm.model.Especialidad;
 
+@Service
+public interface EspecialidadService {
+	public void cargarEspecialidad (Especialidad nuevoEspecialidad);
+	public ArrayList<Especialidad> listarEspecialidades();
+	public Especialidad mostrarUnaEspecialidad(Integer codigo);
+	public Especialidad eliminarUnaEspecialidad(Integer codigo);
+	public void eliminarTodasLasEspecialidades();
+	public Especialidad modificarUnaEspecialidad(Integer codigo);
 }
