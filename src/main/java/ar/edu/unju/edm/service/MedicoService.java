7c83b21d855bc85ebb7bfb85d9bee54aeb761e56
Benja <Benja@10.0.13.189>
2023-06-07 15:20:35 -0300

    Creacion de clases util, service - Tito Benjamin
    - Creada la clase Medicos
    - Realizado el esqueleto de la clase de servicio.

--- src/main/java/ar/edu/unju/edm/service/MedicoService.java
+++ src/main/java/ar/edu/unju/edm/service/MedicoService.java
@@ -1,5 +1,16 @@
 package ar.edu.unju.edm.service;
 
-public class MedicoService {
+import java.util.ArrayList;
+import org.springframework.stereotype.Service;
 
+import ar.edu.unju.edm.model.Medico;
+
+@Service
+public interface MedicoService {
+	public void cargarMedico (Medico nuevoMedico);
+	public ArrayList<Medico> listarMedicos();
+	public Medico mostrarUnMedico(Integer codigo);
+	public Medico eliminarUnMedico(Integer codigo);
+	public void eliminarTodosLosMedicos();
+	public Medico modificarUnMedico(Integer codigo);
 }
--- src/main/java/ar/edu/unju/edm/service/imp/ImpMedicoService.java
+++ src/main/java/ar/edu/unju/edm/service/imp/ImpMedicoService.java
@@ -1,5 +1,41 @@
 package ar.edu.unju.edm.service.imp;
 
-public class ImpMedicoService {
+import java.util.ArrayList;
+import org.springframework.stereotype.Service;
 
+import ar.edu.unju.edm.model.Medico;
+import ar.edu.unju.edm.service.MedicoService;
+import ar.edu.unju.edm.util.Medicos;
+
+@Service
+public class ImpMedicoService implements MedicoService {
+	@Override
+	public void cargarMedico(Medico nuevoMedico) {
+		
+	}
+	
+	@Override
+	public ArrayList<Medico> listarMedicos() {
+		return null;
+	}
+	
+	@Override
+	public Medico mostrarUnMedico(Integer codigo) {
+		return null;
+	}
+	
+	@Override
+	public Medico eliminarUnMedico(Integer codigo) {
+		return null;
+	}
+	
+	@Override
+	public void eliminarTodosLosMedicos() {
+		
+	}
+	
+	@Override
+	public Medico modificarUnMedico(Integer codigo) {
+		return null;
+	}
 }
