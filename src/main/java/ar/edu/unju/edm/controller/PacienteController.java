485255aacef072981aca1b3a80ba1015a6bba043
Dante <facuaylan20@gmail.com>
2023-06-09 11:41:57 -0300

    Creacion del Controller, Repository, Service, html

--- src/main/java/ar/edu/unju/edm/controller/PacienteController.java
+++ src/main/java/ar/edu/unju/edm/controller/PacienteController.java
@@ -1,5 +1,52 @@
 package ar.edu.unju.edm.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.apache.juli.logging.Log;
+import org.apache.juli.logging.LogFactory;
+
+import ar.edu.unju.edm.model.Paciente;
+import ar.edu.unju.edm.service.PacienteService;
+
+@Controller
 public class PacienteController {
+	private static final Log GRUPO1 = LogFactory.getLog(PacienteController.class);
+	
+	@Autowired
+	Paciente paciente;
+	@Autowired
+	@Qualifier
+	PacienteService pacienteService;
+	
+	// Carga del formulario
+	@GetMapping("/formPaciente")
+	public ModelAndView cargarFormpaciente() {
+		
+		ModelAndView vistaFormPaciente = new ModelAndView("formPaciente");
+		vistaFormPaciente.addObject("paciente", paciente);
+		
+		return vistaFormPaciente;
+	}
+	
+	// Carga de la lista
+	@PostMapping("/listaPacientes")
+	public ModelAndView mostrarListaPacientes(@ModelAttribute("paciente") Paciente nuevoPaciente) {
+		
+		ModelAndView vistaListaPacientes = new ModelAndView("listaPacientes");
+		GRUPO1.warn("Mostrando nuevo registro" + nuevoPaciente.getNombres());
+		try {
+			pacienteService.cargarPaciente(nuevoPaciente);
+		} catch (Exception e) {
+			
+		}
+		vistaListaPacientes.addObject("listaPacientes", pacienteService.listarPacientes());
+		
+		return vistaListaPacientes;
+	}
 
 }
--- src/main/java/ar/edu/unju/edm/model/Paciente.java
+++ src/main/java/ar/edu/unju/edm/model/Paciente.java
@@ -1,5 +1,147 @@
+/*Nota: El constructor y los getters reciben la fecha como String y la transforman a LocalDate.*/
 package ar.edu.unju.edm.model;
 
-public class Paciente {
+import java.time.LocalDate;
+import org.springframework.stereotype.Component;
+import jakarta.persistence.Id;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 
+@Component
+@Entity
+public class Paciente {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id_pacientes;
+	private Integer legajo;
+	private String nombres;
+	private String apellidos;
+	private Integer dni; 
+	private String email;
+	private String telefono;
+	private LocalDate fecha_nacimiento;
+	private LocalDate fecha_ingreso;
+	private String domicilio;
+	private Boolean estado;
+	private Integer id_especialidad;
+	
+	public Paciente() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Paciente(Integer id_pacientes, Integer legajo, String nombres, String apellidos, Integer dni, String email, String telefono,
+			String fecha_nacimiento, String fecha_ingreso, String domicilio, Boolean estado,
+			Integer id_especialidad) {
+		super();
+		this.id_pacientes = id_pacientes;
+		this.legajo = legajo;
+		this.nombres = nombres;
+		this.apellidos = apellidos;
+		this.dni = dni;
+		this.email = email;
+		this.telefono = telefono;
+		this.fecha_nacimiento = LocalDate.parse(fecha_nacimiento);
+		this.fecha_ingreso = LocalDate.parse(fecha_ingreso);
+		this.domicilio = domicilio;
+		this.estado = estado;
+		this.id_especialidad = id_especialidad;
+	}
+
+	public Integer getId_pacientes() {
+		return id_pacientes;
+	}
+
+	public void setId_pacientes(Integer id_pacientes) {
+		this.id_pacientes = id_pacientes;
+	}
+
+	public Integer getLegajo() {
+		return legajo;
+	}
+
+	public void setLegajo(Integer legajo) {
+		this.legajo = legajo;
+	}
+
+	public String getNombres() {
+		return nombres;
+	}
+
+	public void setNombres(String nombres) {
+		this.nombres = nombres;
+	}
+
+	public String getApellidos() {
+		return apellidos;
+	}
+
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+	
+	public Integer getDni() {
+		return dni;
+	}
+	
+	public void setDni(Integer dni) {
+		this.dni = dni;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+
+	public LocalDate getFecha_nacimiento() {
+		return fecha_nacimiento;
+	}
+
+	public void setFecha_nacimiento(String fecha_nacimiento) {
+		this.fecha_nacimiento = LocalDate.parse(fecha_nacimiento);
+	}
+
+	public LocalDate getFecha_ingreso() {
+		return fecha_ingreso;
+	}
+
+	public void setFecha_ingreso(String fecha_ingreso) {
+		this.fecha_ingreso = LocalDate.parse(fecha_ingreso);
+	}
+
+	public String getDomicilio() {
+		return domicilio;
+	}
+
+	public void setDomicilio(String domicilio) {
+		this.domicilio = domicilio;
+	}
+
+	public Boolean getEstado() {
+		return estado;
+	}
+
+	public void setEstado(Boolean estado) {
+		this.estado = estado;
+	}
+
+	public Integer getId_especialidad() {
+		return id_especialidad;
+	}
+
+	public void setId_especialidad(Integer id_especialidad) {
+		this.id_especialidad = id_especialidad;
+	}
 }
